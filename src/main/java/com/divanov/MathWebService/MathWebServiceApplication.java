46459327b112858655a5d1b40bfe5a944f07898d
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2022-10-13 23:46:01 +0300

    added new project

---
+++ src/main/java/com/divanov/MathWebService/MathWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.divanov.MathWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/divanov/MathWebService/MathWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divanov.MathWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
