4ae110be3196af67aac9b05b17f418ed015a3a50
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2022-10-17 22:48:16 +0300

    solved problem with request in wsdl

--- src/main/java/com/divanov/MathWebService/endpoint/MathWSEndpoint.java
+++ src/main/java/com/divanov/MathWebService/endpoint/MathWSEndpoint.java
@@ -1,6 +1,6 @@
 package com.divanov.MathWebService.endpoint;
 
-import com.divanov.MathWebService.divanov.ws.math.CreateGetSolutionQuadraticEducation;
+import com.divanov.MathWebService.divanov.ws.math.CreateGetSolutionQuadraticEducationRequest;
 import com.divanov.MathWebService.divanov.ws.math.CreateResponse;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -10,14 +10,13 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 @Endpoint
 public class MathWSEndpoint {
 
+    @PayloadRoot(namespace = "http://math.ws.divanov", localPart = "createGetSolutionQuadraticEducationRequest")
     @ResponsePayload
-    @PayloadRoot(localPart = "createResponse", namespace = "http://math.ws.divanov")
-    public CreateResponse getSolutionQuadraticEducation(@RequestPayload CreateGetSolutionQuadraticEducation request) {
-
+    public CreateResponse createResponse(@RequestPayload CreateGetSolutionQuadraticEducationRequest request) {
         CreateResponse response = new CreateResponse();
+        response.setFormula("Ok");
         response.setX1(request.getA() + request.getB());
         response.setX2(request.getC());
-        response.setFormula("OK");
         return response;
     }
 
