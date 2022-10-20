43857410023be4618b4bee1ff9f7cb30c37d6c5f
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2022-10-20 21:22:11 +0300

    added quadratic education logic

--- src/main/java/com/divanov/MathWebService/endpoint/MathWSEndpoint.java
+++ src/main/java/com/divanov/MathWebService/endpoint/MathWSEndpoint.java
@@ -10,14 +10,64 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 @Endpoint
 public class MathWSEndpoint {
 
+    // TODO Refactor this method!!!
+
+    private static final String ERROR_PARAM_A = "The leading coefficient can't be equals 0";
+    private static final String ERROR_DISCRIMINANT_VALUE = "Discriminant can't be less than 0";
+    private static final String NO_REAL_ROOTS = "The education has no real roots";
+
     @PayloadRoot(namespace = "http://math.ws.divanov", localPart = "createGetSolutionQuadraticEducationRequest")
     @ResponsePayload
-    public CreateResponse createResponse(@RequestPayload CreateGetSolutionQuadraticEducationRequest request) {
+    public CreateResponse createQuadraticEducationResult(@RequestPayload CreateGetSolutionQuadraticEducationRequest request) {
         CreateResponse response = new CreateResponse();
-        response.setFormula("Ok");
-        response.setX1(request.getA() + request.getB());
-        response.setX2(request.getC());
-        return response;
+
+        if (request.getA() != 0) {
+            // Incomplete quadratic education
+            if (request.getB() == 0 && request.getC() == 0) {
+                response.setFormula(generateEducationFormula(request.getA(), request.getB(), request.getC()));
+                response.setX1(0.0);
+                return response;
+            } else if (request.getB() == 0 && request.getC() != 0) {
+                response.setFormula(generateEducationFormula(request.getA(), request.getB(), request.getC()));
+                if (-(request.getC() / request.getA()) > 0) {
+                    response.setX1(Math.sqrt(-(request.getC() / request.getA())));
+                    response.setX2(-(Math.sqrt(-(request.getC() / request.getA()))));
+                    return response;
+                } else {
+                    throw new ArithmeticException(NO_REAL_ROOTS);
+                }
+            }
+
+            // Complete quadratic education
+            response.setD(Math.pow(request.getB(), 2) - 4 * request.getA() * request.getC());
+            if (response.getD() > 0) {
+                response.setFormula(generateEducationFormula(request.getA(), request.getB(), request.getC()));
+                response.setX1((-request.getB() + Math.sqrt(response.getD())) / (2 * request.getA()));
+                response.setX2((-request.getB() - Math.sqrt(response.getD())) / (2 * request.getA()));
+                return response;
+            } else if (response.getD() == 0) {
+                response.setFormula(generateEducationFormula(request.getA(), request.getB(), request.getC()));
+                response.setX1(-request.getB() / (2 * request.getA()));
+                return response;
+            }
+
+            // custom fault message(Formula and value of D)
+            throw new ArithmeticException(ERROR_DISCRIMINANT_VALUE);
+
+        } else {
+            throw new ArithmeticException(ERROR_PARAM_A);
+        }
+    }
+
+    private static String generateEducationFormula(double a, double b, double c) {
+        if (b == 0 && c == 0) {
+            return String.format("%.1fx^2 = 0", a);
+        } else if (b == 0 && c != 0) {
+            return String.format("%.1fx^2 + %.1f = 0", a, c);
+        } else if (b != 0 && c == 0) {
+            return String.format("%.1fx^2 + %.1fx = 0", a, b);
+        }
+        return String.format("%.1fx^2 + %.1fx + %.1f = 0", a, b, c);
     }
 
 }
