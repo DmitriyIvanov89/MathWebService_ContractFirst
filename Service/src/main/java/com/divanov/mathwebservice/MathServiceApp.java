ccde987dad1241f34b87cb0aeb445b4ac25a9c99
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2022-11-02 00:06:16 +0300

    mod project structure for multymodules

--- Service/src/main/java/com/divanov/mathwebservice/MathServiceApp.java
+++ Service/src/main/java/com/divanov/mathwebservice/MathServiceApp.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MathWebServiceApplication {
+public class MathServiceApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MathWebServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MathServiceApp.class, args);
+    }
 }
