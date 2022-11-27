e0fdb23c3821693fb99934c12c7a1660331ebfcb
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2022-11-27 23:51:33 +0300

    added controller

--- Service/src/main/java/com/divanov/mathwebservice/MathServiceApp.java
+++ Service/src/main/java/com/divanov/mathwebservice/MathServiceApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MathServiceApp {
-
     public static void main(String[] args) {
         SpringApplication.run(MathServiceApp.class, args);
     }
