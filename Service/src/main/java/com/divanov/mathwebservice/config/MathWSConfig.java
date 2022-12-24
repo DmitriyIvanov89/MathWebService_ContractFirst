61c0bb5fa85d441fd79e50699087c8de754f2549
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2022-12-24 12:55:33 +0300

    added first test

--- Service/src/main/java/com/divanov/mathwebservice/config/MathWSConfig.java
+++ Service/src/main/java/com/divanov/mathwebservice/config/MathWSConfig.java
@@ -28,7 +28,6 @@ public class MathWSConfig extends WsConfigurerAdapter {
         servlet.setApplicationContext(applicationContext);
         servlet.setTransformWsdlLocations(true);
         return new ServletRegistrationBean<>(servlet, "/services/*");
-//        return new ServletRegistrationBean<>(servlet, "/api/*");
     }
 
     @Bean(name = "MathService")
@@ -38,7 +37,6 @@ public class MathWSConfig extends WsConfigurerAdapter {
         wsdl11Definition.setPortTypeName("MathPort");
         wsdl11Definition.setTargetNamespace("http://math.ws.divanov");
         wsdl11Definition.setLocationUri("/services/MathServiceEndpoint");
-//        wsdl11Definition.setLocationUri("/api/calc");
         wsdl11Definition.setSchema(schema());
         return wsdl11Definition;
     }
