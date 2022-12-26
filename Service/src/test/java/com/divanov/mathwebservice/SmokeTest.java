94c59ede78892782632c5f4218f2f0563d7ff4c1
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2022-12-26 23:50:39 +0300

    before restruct

---
+++ Service/src/test/java/com/divanov/mathwebservice/SmokeTest.java
@@ -0,0 +1,19 @@
+package com.divanov.mathwebservice;
+
+import com.divanov.mathwebservice.controller.MathWSController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class SmokeTest {
+    @Autowired
+    private MathWSController controller;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+}
---
+++ Service/src/test/java/com/divanov/mathwebservice/TestMathService.java
@@ -0,0 +1,7 @@
+package com.divanov.mathwebservice;
+
+import org.junit.runner.RunWith;
+
+public class TestMathService {
+
+}
