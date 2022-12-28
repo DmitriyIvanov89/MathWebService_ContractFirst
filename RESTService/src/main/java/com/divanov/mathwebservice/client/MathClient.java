35030cbe73c39fa66a26be03e6b0b64b1016b1cc
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2022-12-28 23:15:41 +0300

    mod RESTService

--- RESTService/src/main/java/com/divanov/mathwebservice/client/MathClient.java
+++ RESTService/src/main/java/com/divanov/mathwebservice/client/MathClient.java
@@ -1,50 +1,42 @@
 package com.divanov.mathwebservice.client;
 
-import com.divanov.mathwebservice.dto.QuadraticEducationResponse;
-import com.divanov.mathwebservice.exception.ServiceException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
-import reactor.core.publisher.Mono;
-
 public class MathClient {
-    private final static String QUADRATIC_EDUCATION_RESOURCE= "/api/calc";
-
-    @Autowired
-    WebClient webClient;
-
-    public QuadraticEducationResponse getQuadraticEducationResult(double a, double b, double c) {
-        try {
-            return webClient
-                    .get()
-                    .uri(uriBuilder -> uriBuilder
-                            .path(QUADRATIC_EDUCATION_RESOURCE)
-                            .queryParam("a", a)
-                            .queryParam("b", b)
-                            .queryParam("c", c)
-                            .build())
-                    .retrieve()
-                    .bodyToMono(QuadraticEducationResponse.class)
-                    .block();
-        } catch (WebClientResponseException we) {
-            throw new ServiceException(we.getMessage(), we.getRawStatusCode());
-        }
-    }
-
-    /**
-     * This method is described in
-     * <a href="https://careydevelopment.us/blog/spring-webflux-how-to-handle-errors-with-webclient">...</a>
-     */
-
-    private Mono<ClientResponse> logBody(ClientResponse response) {
-        if (response.statusCode().is4xxClientError() || response.statusCode().is5xxServerError()) {
-            return response.bodyToMono(String.class)
-                    .flatMap(body -> {
-                        return Mono.error(new ServiceException(body, response.rawStatusCode()));
-                    });
-        } else {
-            return Mono.just(response);
-        }
-    }
+//    private final static String QUADRATIC_EDUCATION_RESOURCE= "/api/calc";
+//
+//    @Autowired
+//    WebClient webClient;
+//
+//    public QuadraticEducationResponse getQuadraticEducationResult(double a, double b, double c) {
+//        try {
+//            return webClient
+//                    .get()
+//                    .uri(uriBuilder -> uriBuilder
+//                            .path(QUADRATIC_EDUCATION_RESOURCE)
+//                            .queryParam("a", a)
+//                            .queryParam("b", b)
+//                            .queryParam("c", c)
+//                            .build())
+//                    .retrieve()
+//                    .bodyToMono(QuadraticEducationResponse.class)
+//                    .block();
+//        } catch (WebClientResponseException we) {
+//            throw new ServiceException(we.getMessage(), we.getRawStatusCode());
+//        }
+//    }
+//
+//    /**
+//     * This method is described in
+//     * <a href="https://careydevelopment.us/blog/spring-webflux-how-to-handle-errors-with-webclient">...</a>
+//     */
+//
+//    private Mono<ClientResponse> logBody(ClientResponse response) {
+//        if (response.statusCode().is4xxClientError() || response.statusCode().is5xxServerError()) {
+//            return response.bodyToMono(String.class)
+//                    .flatMap(body -> {
+//                        return Mono.error(new ServiceException(body, response.rawStatusCode()));
+//                    });
+//        } else {
+//            return Mono.just(response);
+//        }
+//    }
 }
