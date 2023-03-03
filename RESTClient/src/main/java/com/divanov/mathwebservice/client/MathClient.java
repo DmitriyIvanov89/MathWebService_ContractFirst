a94a8b824775c576399596487a3891151748dad3
DmitriyIvanov <dmitriy.a.ivanov@rtlabs.ru>
2023-03-03 09:14:32 +0300

    mod client

--- RESTClient/src/main/java/com/divanov/mathwebservice/client/MathClient.java
+++ RESTClient/src/main/java/com/divanov/mathwebservice/client/MathClient.java
@@ -1,18 +1,16 @@
 package com.divanov.mathwebservice.client;
 
-import com.divanov.mathwebservice.dto.ObjectFactory;
-import com.divanov.mathwebservice.dto.QuadraticEducationRequest;
-import com.divanov.mathwebservice.dto.QuadraticEducationResponse;
+import com.divanov.mathwebservice.ge
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 public class MathClient extends WebServiceGatewaySupport {
 
-    public QuadraticEducationResponse getQuadraticEducationSolution(double a, double b, double c) {
-        ObjectFactory objectFactory = new ObjectFactory();
-        QuadraticEducationRequest request = objectFactory.createQuadraticEducationRequest();
-        request.setA(a);
-        request.setB(b);
-        request.setC(c);
-        return (QuadraticEducationResponse) getWebServiceTemplate().marshalSendAndReceive(request);
-    }
+//    public QuadraticEducationResponse getQuadraticEducationSolution(double a, double b, double c) {
+//        ObjectFactory objectFactory = new ObjectFactory();
+//        QuadraticEducationRequest request = objectFactory.createQuadraticEducationRequest();
+//        request.setA(a);
+//        request.setB(b);
+//        request.setC(c);
+//        return (QuadraticEducationResponse) getWebServiceTemplate().marshalSendAndReceive(request);
+//    }
 }
--- RESTClient/src/main/java/com/divanov/mathwebservice/controller/MathWSController.java
+++ RESTClient/src/main/java/com/divanov/mathwebservice/controller/MathWSController.java
@@ -1,22 +1,22 @@
 package com.divanov.mathwebservice.controller;
-
-import com.divanov.mathwebservice.client.MathClient;
-import com.divanov.mathwebservice.dto.*;
-import org.springframework.beans.factory.annotation.Autowired;
+//
+//import com.divanov.mathwebservice.client.MathClient;
+//import com.divanov.mathwebservice.dto.*;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/calc")
 public class MathWSController {
 
-    @Autowired
-    private MathClient mathClient;
-
-    @GetMapping
-    public QuadraticEducationResponse getResult(@RequestParam double a,
-                                                @RequestParam double b,
-                                                @RequestParam double c) {
-
-        return mathClient.getQuadraticEducationSolution(a, b, c);
-    }
+//    @Autowired
+//    private MathClient mathClient;
+//
+//    @GetMapping
+//    public QuadraticEducationResponse getResult(@RequestParam double a,
+//                                                @RequestParam double b,
+//                                                @RequestParam double c) {
+//
+//        return mathClient.getQuadraticEducationSolution(a, b, c);
+//    }
 }
