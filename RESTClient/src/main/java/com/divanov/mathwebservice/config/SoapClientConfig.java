1cb4ac1b38ef0e194b11cfe91d8ebfc67495c3c1
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2023-03-06 23:18:38 +0300

    mod

--- RESTClient/src/main/java/com/divanov/mathwebservice/config/SoapClientConfig.java
+++ RESTClient/src/main/java/com/divanov/mathwebservice/config/SoapClientConfig.java
@@ -1,12 +1,8 @@
 package com.divanov.mathwebservice.config;
 
-;
 import com.divanov.mathwebservice.client.MathPortSoap11Impl;
-<<<<<<< HEAD
 import com.divanov.mathwebservice.generated.MathService;
 import com.divanov.mathwebservice.generated.ObjectFactory;
-=======
->>>>>>> 6cd713248a87e914ee87c21dae6e24827b296e01
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,33 +19,6 @@ public class SoapClientConfig {
     public MathPortSoap11Impl mathPortSoap11() {
         return new MathPortSoap11Impl(service());
     }
-
-//    @Bean
-//    public Jaxb2Marshaller marshaller() {
-//        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-//        // this is the package name specified in the <generatePackage>
-//        marshaller.setContextPath("com.divanov.mathwebservice.dto");
-//        return marshaller;
-//    }
-
-//    @Bean
-//    public MathClient mathClient(Jaxb2Marshaller jaxb2Marshaller) {
-//        MathClient client = new MathClient();
-//        client.setDefaultUri("http://localhost:8080/services/MathServiceEndpoint");
-//        client.setMarshaller(jaxb2Marshaller);
-//        client.setUnmarshaller(jaxb2Marshaller);
-//        return client;
-//    }
-
-//    @Bean
-//    public MathClient mathClient(Jaxb2Marshaller jaxb2Marshaller) {
-//        MathClient client = new MathClient();
-//        client.setDefaultUri("http://localhost:8080/services/MathServiceEndpoint");
-//        client.setMarshaller(jaxb2Marshaller);
-//        client.setUnmarshaller(jaxb2Marshaller);
-//        return client;
-//    }
-
 
     @Bean
     public ObjectFactory objectFactory() {
--- RESTClient/src/main/java/com/divanov/mathwebservice/controller/MathWSController.java
+++ RESTClient/src/main/java/com/divanov/mathwebservice/controller/MathWSController.java
@@ -1,10 +1,7 @@
 package com.divanov.mathwebservice.controller;
 
 import com.divanov.mathwebservice.client.MathPortSoap11Impl;
-<<<<<<< HEAD
 import com.divanov.mathwebservice.generated.*;
-=======
->>>>>>> 6cd713248a87e914ee87c21dae6e24827b296e01
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,21 +9,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/calc")
 public class MathWSController {
 
-//    @Autowired
-//    private MathPortSoap11Impl mathPortSoap11;
-//
-//    @Autowired
-//    private ObjectFactory objectFactory;
-//
-//    @GetMapping
-//    public QuadraticEducationResponse getResult(@RequestParam double a,
-//                                                @RequestParam double b,
-//                                                @RequestParam double c) throws QuadraticEducationFault_Exception {
-//        QuadraticEducationRequest request = objectFactory.createQuadraticEducationRequest();
-//        request.setA(a);
-//        request.setB(b);
-//        request.setC(c);
-//
-//        return mathPortSoap11.quadraticEducation(request);
-//    }
+    @Autowired
+    private MathPortSoap11Impl mathPortSoap11;
+
+    @Autowired
+    private ObjectFactory objectFactory;
+
+    @GetMapping
+    public SolveQuadraticEducationResponse getResult(@RequestParam double a,
+                                                     @RequestParam double b,
+                                                     @RequestParam double c) {
+        SolveQuadraticEducationRequest request = objectFactory.createSolveQuadraticEducationRequest();
+        request.setA(a);
+        request.setB(b);
+        request.setC(c);
+
+        return mathPortSoap11.solveQuadraticEducation(request);
+    }
 }
