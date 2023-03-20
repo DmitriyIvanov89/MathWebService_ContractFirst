2e86a340885a689ef737f08ba3abeda9e1ef28d8
DmitriyIvanov <dmitriy.a.ivanov@rtlabs.ru>
2023-03-20 17:35:32 +0300

    mod

--- SoapService/src/main/java/com/divanov/mathwebservice/config/MathWSConfig.java
+++ SoapService/src/main/java/com/divanov/mathwebservice/config/MathWSConfig.java
@@ -48,7 +48,7 @@ public class MathWSConfig extends WsConfigurerAdapter {
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
         wsdl11Definition.setPortTypeName("MathService");
         wsdl11Definition.setSchema(schema);
-        wsdl11Definition.setFaultSuffix("commonFault");
+
         wsdl11Definition.setLocationUri("/services/MathServiceEndpoint");
         return wsdl11Definition;
     }
