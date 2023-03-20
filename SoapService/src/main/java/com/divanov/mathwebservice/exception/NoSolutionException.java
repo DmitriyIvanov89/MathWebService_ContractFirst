1ba50cabe2e20a6a08805d6e59fed71be7e56a3b
DmitriyIvanov <dmitriy.a.ivanov@rtlabs.ru>
2023-03-20 16:39:28 +0300

    mod exceptions

--- SoapService/src/main/java/com/divanov/mathwebservice/exception/NoSolutionException.java
+++ SoapService/src/main/java/com/divanov/mathwebservice/exception/NoSolutionException.java
@@ -1,12 +1,11 @@
 package com.divanov.mathwebservice.exception;
 
+import com.divanov.mathwebservice.service.MathService;
 import org.springframework.ws.soap.server.endpoint.annotation.FaultCode;
 import org.springframework.ws.soap.server.endpoint.annotation.SoapFault;
 
-@SoapFault(faultCode = FaultCode.CUSTOM, customFaultCode = "{" + NoSolutionException.NAMESPACE_URI + "} NO_SOLUTION_EXCEPTION")
+@SoapFault(faultCode = FaultCode.CUSTOM, customFaultCode = "{" + MathService.NAME_SPACE + "} NO_SOLUTION_EXCEPTION")
 public class NoSolutionException extends MathServiceException {
-
-    public static final String NAMESPACE_URI = "http://math.ws.divanov";
 
     public NoSolutionException(String message) {
         super(message);
--- SoapService/src/main/java/com/divanov/mathwebservice/exception/RequestValidationException.java
+++ SoapService/src/main/java/com/divanov/mathwebservice/exception/RequestValidationException.java
@@ -1,5 +1,6 @@
 package com.divanov.mathwebservice.exception;
 
+import com.divanov.mathwebservice.service.MathService;
 import org.springframework.ws.soap.server.endpoint.annotation.FaultCode;
 import org.springframework.ws.soap.server.endpoint.annotation.SoapFault;
 
@@ -7,11 +8,8 @@ import org.springframework.ws.soap.server.endpoint.annotation.SoapFault;
  * Exception in case of invalid data in payload
  */
 
-@SoapFault(faultCode = FaultCode.CUSTOM, customFaultCode = "{" + RequestValidationException.NAMESPACE_URI + "}VALIDATION_ERROR")
+@SoapFault(faultCode = FaultCode.CUSTOM, customFaultCode = "{" + MathService.NAME_SPACE + "}VALIDATION_ERROR")
 public class RequestValidationException extends MathServiceException {
-
-    private static final long serialVersionUID = 1L;
-    public static final String NAMESPACE_URI = "http://math.ws.divanov";
 
     public RequestValidationException(String message) {
         super(message);
