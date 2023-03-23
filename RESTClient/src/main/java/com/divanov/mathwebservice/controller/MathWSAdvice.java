5b1a4b2466a23575836484ac6a76c833cb444e09
DmitriyIvanov <dmitriy.a.ivanov@rtlabs.ru>
2023-03-23 09:35:44 +0300

    mod advicecontrollerAdvice

--- RESTClient/src/main/java/com/divanov/mathwebservice/controller/MathWSAdvice.java
+++ RESTClient/src/main/java/com/divanov/mathwebservice/controller/MathWSAdvice.java
@@ -2,6 +2,7 @@ package com.divanov.mathwebservice.controller;
 
 import com.divanov.mathwebservice.gen.ErrorFault;
 import com.divanov.mathwebservice.gen.QuadraticEducationException;
+import org.apache.cxf.binding.soap.SoapFault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,5 +18,10 @@ public class MathWSAdvice {
         errorFault.setFormula(exception.getFaultInfo().getFormula());
         errorFault.setDiscriminant(exception.getFaultInfo().getDiscriminant());
         return new ResponseEntity<>(errorFault, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(org.apache.cxf.binding.soap.SoapFault.class)
+    public ResponseEntity<String> handleSoapFault(SoapFault exception) {
+        return new ResponseEntity<>("Message: " + exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
