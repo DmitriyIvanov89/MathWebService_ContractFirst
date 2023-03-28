5eca719e069c21eeab2ad8facf03a4bf440c928c
DmitriyIvanov <dmitriy.a.ivanov@rtlabs.ru>
2023-03-28 16:38:48 +0300

    mod trests

--- RESTClient/src/test/java/com/divanov/mathwebservice/controller/MathControllerTest.java
+++ RESTClient/src/test/java/com/divanov/mathwebservice/controller/MathControllerTest.java
@@ -1,53 +1,42 @@
 package com.divanov.mathwebservice.controller;
 
-import com.divanov.mathwebservice.gen.MathServiceService;
 import com.divanov.mathwebservice.gen.QuadraticEducationException;
-import com.divanov.mathwebservice.gen.QuadraticEducationRequestPayLoad;
 import com.divanov.mathwebservice.gen.SolutionQuadraticEducation;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//@SpringJUnitConfig
-//@ContextConfiguration(classes = MathComponentTestConfig.class)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = MathControllerTestConfig.class)
 class MathControllerTest {
+
+    /**
+     * Comparison of classes by individual fields
+     */
+
+    @Autowired
+    MathController controller;
 
     @Test
     void should_work() throws QuadraticEducationException {
-        MathController controller = new MathController(new MathServiceService());
-        SolutionQuadraticEducation actualResponse = controller.getResult(2, -3, 1);
         SolutionQuadraticEducation expectedResponse = new SolutionQuadraticEducation();
         expectedResponse.setFormula("2,0x^2 + -3,0x + 1,0 = 0");
         expectedResponse.setDiscriminant(1.0);
         expectedResponse.setX1(1.0);
         expectedResponse.setX2(0.5);
-        assertEquals(expectedResponse, actualResponse);
 
+        SolutionQuadraticEducation actualResponse = controller.getResult(2, -3, 1);
+
+        assertAll(
+                () -> assertEquals(expectedResponse.getFormula(), actualResponse.getFormula()),
+                () -> assertEquals(expectedResponse.getDiscriminant(), actualResponse.getDiscriminant()),
+                () -> assertEquals(expectedResponse.getX1(), actualResponse.getX1()),
+                () -> assertEquals(expectedResponse.getX2(), actualResponse.getX2())
+        );
     }
-
-//    @Test
-//    void shouldReturnCorrectAnswer() throws QuadraticEducationException {
-//        SolutionQuadraticEducation expectedResponse = new SolutionQuadraticEducation();
-//        expectedResponse.setFormula("2,0x^2 + -3,0x + 1,0 = 0");
-//        expectedResponse.setDiscriminant(1.0);
-//        expectedResponse.setX1(1.0);
-//        expectedResponse.setX2(0.5);
-//
-//        Mockito.when(controller.getResult(2, -3, 1)).thenReturn(expectedResponse);
-//
-//        SolutionQuadraticEducation actualResponse = controller.getResult(2, -3, 1);
-//        assertEquals(expectedResponse, actualResponse);
-//
-//    }
-
-
 }
---
+++ RESTClient/src/test/java/com/divanov/mathwebservice/controller/MathControllerTestConfig.java
@@ -0,0 +1,16 @@
+package com.divanov.mathwebservice.controller;
+
+import com.divanov.mathwebservice.gen.MathServiceService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.divanov.mathwebservice")
+public class MathControllerTestConfig {
+
+    @Bean
+    public MathController mathController(MathServiceService mathServiceService) {
+        return new MathController(mathServiceService);
+    }
+}
