c4ca975ef82a1545e9836ac0a91a6e51128bcd9e
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2023-03-28 22:11:38 +0300

    mod test

--- RESTClient/src/test/java/com/divanov/mathwebservice/controller/MathControllerTest.java
+++ RESTClient/src/test/java/com/divanov/mathwebservice/controller/MathControllerTest.java
@@ -1,12 +1,12 @@
 package com.divanov.mathwebservice.controller;
 
-;
+import com.divanov.mathwebservice.gen.ObjectFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;;
 import com.divanov.mathwebservice.gen.QuadraticEducationException;
 import com.divanov.mathwebservice.gen.SolutionQuadraticEducation;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = MathControllerTestConfig.class)
+@SpringBootTest
 class MathControllerTest {
 
     /**
@@ -24,13 +24,13 @@ class MathControllerTest {
     @Autowired
     MathController controller;
 
+    @Autowired
+    ObjectFactory objectFactory;
+
     @Test
-    void should_work() throws QuadraticEducationException {
-        SolutionQuadraticEducation expectedResponse = new SolutionQuadraticEducation();
-        expectedResponse.setFormula("2,0x^2 + -3,0x + 1,0 = 0");
-        expectedResponse.setDiscriminant(1.0);
-        expectedResponse.setX1(1.0);
-        expectedResponse.setX2(0.5);
+    void shouldReturnCorrectResponse() throws QuadraticEducationException {
+        SolutionQuadraticEducation expectedResponse = createResponse("2,0x^2 + -3,0x + 1,0 = 0",
+                1.0, 1.0, 0.5);
 
         SolutionQuadraticEducation actualResponse = controller.getResult(2, -3, 1);
 
@@ -40,5 +40,14 @@ class MathControllerTest {
                 () -> assertEquals(expectedResponse.getX1(), actualResponse.getX1()),
                 () -> assertEquals(expectedResponse.getX2(), actualResponse.getX2())
         );
+    }
+
+    private SolutionQuadraticEducation createResponse(String formula, double discriminant, double x1, Double x2) {
+        SolutionQuadraticEducation response = objectFactory.createSolutionQuadraticEducation();
+        response.setFormula(formula);
+        response.setDiscriminant(discriminant);
+        response.setX1(x1);
+        response.setX2(x2);
+        return response;
     }
 }
