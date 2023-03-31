d34295c65c844e42da4a95fb91ee24a622ba25b4
DmitriyIvanov <dmitriy.a.ivanov@rtlabs.ru>
2023-03-31 09:47:14 +0300

    mod tests for rest client

--- RESTClient/src/test/java/com/divanov/mathwebservice/controller/controller/MathControllerTest.java
+++ RESTClient/src/test/java/com/divanov/mathwebservice/controller/controller/MathControllerTest.java
@@ -1,7 +1,6 @@
 package com.divanov.mathwebservice.controller.controller;
 
 import com.divanov.mathwebservice.controller.MathController;
-import com.divanov.mathwebservice.gen.ErrorResponse;
 import com.divanov.mathwebservice.gen.ObjectFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;;
@@ -16,10 +15,6 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class MathControllerTest {
 
-    /**
-     * Comparison of classes by individual fields
-     */
-
     @Autowired
     MathController controller;
 
@@ -28,50 +23,38 @@ class MathControllerTest {
 
     @Test
     void shouldReturnCorrectResponse() throws QuadraticEducationException {
-        SolutionQuadraticEducation expectedResponse = createResponse("2,0x^2 + -3,0x + 1,0 = 0",
-                1.0, 1.0, 0.5);
-
         SolutionQuadraticEducation actualResponse = controller.getResult(2, -3, 1);
 
         assertAll(
-                () -> assertEquals(expectedResponse.getFormula(), actualResponse.getFormula()),
-                () -> assertEquals(expectedResponse.getDiscriminant(), actualResponse.getDiscriminant()),
-                () -> assertEquals(expectedResponse.getX1(), actualResponse.getX1()),
-                () -> assertEquals(expectedResponse.getX2(), actualResponse.getX2())
+                () -> assertEquals("2,0x^2 + -3,0x + 1,0 = 0", actualResponse.getFormula()),
+                () -> assertEquals(1.0, actualResponse.getDiscriminant()),
+                () -> assertEquals(1.0, actualResponse.getX1()),
+                () -> assertEquals(0.5, actualResponse.getX2())
         );
     }
 
     @Test
     void shouldReturnResponseWithOneRoot() throws QuadraticEducationException {
-        SolutionQuadraticEducation expectedResponse = createResponse("1,0x^2 + -6,0x + 9,0 = 0",
-                0.0, 3.0, null);
-
         SolutionQuadraticEducation actualResponse = controller.getResult(1, -6, 9);
 
         assertAll(
-                () -> assertEquals(expectedResponse.getFormula(), actualResponse.getFormula()),
-                () -> assertEquals(expectedResponse.getDiscriminant(), actualResponse.getDiscriminant()),
-                () -> assertEquals(expectedResponse.getX1(), actualResponse.getX1()),
+                () -> assertEquals("1,0x^2 + -6,0x + 9,0 = 0", actualResponse.getFormula()),
+                () -> assertEquals(0.0, actualResponse.getDiscriminant()),
+                () -> assertEquals(3.0, actualResponse.getX1()),
                 () -> assertNull(actualResponse.getX2())
         );
     }
 
     @Test
     void shouldReturnExceptionsWithDetails() {
-        ErrorResponse expectedErrorResponse = objectFactory.createErrorResponse();
-        expectedErrorResponse.setMessage("Discriminant can't be less than 0");
-        expectedErrorResponse.setDiscriminant(-131.0);
-        expectedErrorResponse.setFormula("5,0x^2 + 3,0x + 7,0 = 0");
-
-
         QuadraticEducationException actualException = assertThrows(
                 QuadraticEducationException.class, () -> controller.getResult(5, 3, 7)
         );
 
         assertAll(
-                () -> assertEquals(expectedErrorResponse.getMessage(), actualException.getMessage()),
-                () -> assertEquals(expectedErrorResponse.getDiscriminant(), actualException.getFaultInfo().getDiscriminant()),
-                () -> assertEquals(expectedErrorResponse.getFormula(), actualException.getFaultInfo().getFormula())
+                () -> assertEquals("Discriminant can't be less than 0", actualException.getMessage()),
+                () -> assertEquals("5,0x^2 + 3,0x + 7,0 = 0", actualException.getFaultInfo().getFormula()),
+                () -> assertEquals(-131.0, actualException.getFaultInfo().getDiscriminant())
         );
     }
 
@@ -95,27 +78,25 @@ class MathControllerTest {
 
     @Test
     void shouldReturnResponseIncompleteEducationWhenCEqualsZero() throws QuadraticEducationException {
-        SolutionQuadraticEducation expectedResponse = createResponse("2,0x^2 + -3,0x = 0", 0, 0, 1.5);
         SolutionQuadraticEducation actualResponse = controller.getResult(2, -3, 0);
 
         assertAll(
-                () -> assertEquals(expectedResponse.getFormula(), actualResponse.getFormula()),
-                () -> assertEquals(expectedResponse.getDiscriminant(), actualResponse.getDiscriminant()),
-                () -> assertEquals(expectedResponse.getX1(), actualResponse.getX1()),
-                () -> assertEquals(expectedResponse.getX2(), actualResponse.getX2())
+                () -> assertEquals("2,0x^2 + -3,0x = 0", actualResponse.getFormula()),
+                () -> assertEquals(0, actualResponse.getDiscriminant()),
+                () -> assertEquals(0, actualResponse.getX1()),
+                () -> assertEquals(1.5, actualResponse.getX2())
         );
     }
 
     @Test
     void shouldReturnResponseIncompleteEducationWhenBEqualsZero() throws QuadraticEducationException {
-        SolutionQuadraticEducation expectedResponse = createResponse("4,0x^2 + -9,0 = 0", 0, 1.5, -1.5);
         SolutionQuadraticEducation actualResponse = controller.getResult(4, 0, -9);
 
         assertAll(
-                () -> assertEquals(expectedResponse.getFormula(), actualResponse.getFormula()),
-                () -> assertEquals(expectedResponse.getDiscriminant(), actualResponse.getDiscriminant()),
-                () -> assertEquals(expectedResponse.getX1(), actualResponse.getX1()),
-                () -> assertEquals(expectedResponse.getX2(), actualResponse.getX2())
+                () -> assertEquals("4,0x^2 + -9,0 = 0", actualResponse.getFormula()),
+                () -> assertEquals(0, actualResponse.getDiscriminant()),
+                () -> assertEquals(1.5, actualResponse.getX1()),
+                () -> assertEquals(-1.5, actualResponse.getX2())
         );
     }
 
