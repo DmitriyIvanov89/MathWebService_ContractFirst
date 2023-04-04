095a18e5bd19a439aa7f5fb1900e99d877e64a32
DmitriyIvanov <dmitriy.a.ivanov@rtlabs.ru>
2023-04-04 17:05:30 +0300

    start add log app

--- SoapService/src/main/java/com/divanov/mathwebservice/validatorinterceptor/ValidationInterceptorMathWS.java
+++ SoapService/src/main/java/com/divanov/mathwebservice/validatorinterceptor/ValidationInterceptorMathWS.java
@@ -1,6 +1,10 @@
 package com.divanov.mathwebservice.validatorinterceptor;
 
 import com.divanov.mathwebservice.service.exception.RequestValidationException;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.ws.context.MessageContext;
 import org.springframework.ws.soap.server.endpoint.interceptor.PayloadValidatingInterceptor;
 import org.xml.sax.SAXException;
@@ -10,14 +14,12 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class ValidationInterceptorMathWS extends PayloadValidatingInterceptor {
+    private static final Logger log = LogManager.getRootLogger();
 
     @Override
     protected boolean handleRequestValidationErrors(MessageContext messageContext, SAXParseException[] errors) {
         if (errors.length > 0) {
-//            String validationsErrorString = Arrays.stream(errors)
-//                    .map(error -> "[" + error.getLineNumber() + ", " + error.getColumnNumber() + "]: " + error.getMessage())
-//                    .collect(Collectors.joining(" -- "));
-
+            log.info("start logger");
             String validationsErrorString = Arrays.stream(errors)
                     .map(SAXException::getMessage)
                     .collect(Collectors.joining(" -- "));
