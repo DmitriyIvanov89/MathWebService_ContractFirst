3c42bb4cd9fa63e3387c6cf1db8cbbe67785a4a7
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2023-04-25 09:05:45 +0300

    mod

--- SoapService/src/main/java/com/divanov/mathwebservice/service/imp/MathServiceImpl.java
+++ SoapService/src/main/java/com/divanov/mathwebservice/service/imp/MathServiceImpl.java
@@ -9,8 +9,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.Locale;
 
 @Service
